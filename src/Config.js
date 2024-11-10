d7b240b2beb8a081cf708416ffecedfc1aaad7d1
shahar9840 <92571880+shahar9840@users.noreply.github.com>
2024-11-10 12:06:49 +0200

    50

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 // src/config.js
-const config2 = {
+const config = {
     apiUrl: 'https://moveotask-back-2.onrender.com'
   };
-const config = {
+const config2 = {
     apiUrl: 'http://localhost:5001'
   };
   
